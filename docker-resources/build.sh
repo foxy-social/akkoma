5894bcfefce93651477e745a5b3d83b5f04bbaf4
Luna D <luna@nighty.cloud>
2023-06-29 21:23:54 +0200

    akkoma is kind of trash

--- docker-resources/build.sh
+++ docker-resources/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-docker-compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) akkoma
-docker-compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) db
+docker-compose build --build-arg UID=$(id -u) --build-arg GID=131 akkoma
+docker-compose build --build-arg UID=$(id -u) --build-arg GID=131 db
+# docker-compose run --rm -d --user root --name akkoma-db-install db
+# docker exec -it akkoma-db-install /usr/local/bin/setup-db.sh
+# docker kill akkoma-db-install
+# docker rm akkoma-db-install
